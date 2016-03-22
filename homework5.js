976a03ce5ac8d19dbed6675af9d20e46e461a96f
gargimk <gargimrunalkulkarni@csu.fullerton.edu>
2016-03-21 22:53:06 -0700

    Homework Assignment 5
    Homework Assignment 5
    Submitted by:
    Niyati Bichu (CWID: 893471748)
    Gargi Mrunal Kulkarni (CWID: 893210922)

---
+++ homework5.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var wins=0,
+losses=0,
+result;
+
+app.use(bodyParser.json());
+
+app.post('/flip', function (req, res) {
+    var myChoice = req.body.call;
+    console.log("My choice : " + myChoice);
+    var coinSide = getCoinSide();
+	console.log("Coin side : " + coinSide)
+    if (myChoice === coinSide){
+        wins++;
+		result="win";
+		console.log("Result: " + result);
+		console.log("wins: " + wins + " losses: "+ losses);
+        res.end(JSON.stringify({
+            "result": "win"
+        }));
+    }
+	else{
+		losses++;
+		result="loss";
+		console.log("Result: " + result);
+		console.log("wins: " + wins + " losses: "+ losses);
+        res.end(JSON.stringify({
+            "result": "loss"
+        }));
+	}
+
+});
+
+app.get('/stats', function (req, res) {
+  res.sendStatus(JSON.stringify({
+        "wins": wins,
+        "losses": losses
+    }));
+});
+
+function getCoinSide(){
+   var side = Math.random();
+   if(side<0.5){
+       return "heads";
+   }
+   else{
+       return "tails";
+   }
+}
+
+app.listen(3000, function() {
+    console.log('App running on port 3000!');
+});
